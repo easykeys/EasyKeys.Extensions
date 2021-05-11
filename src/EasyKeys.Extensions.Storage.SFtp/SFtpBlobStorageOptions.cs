1d4b93c45dc961a65b366e69aa9cda5afe76f157
kdcllc <info@kingdavidconsulting.com>
2021-05-10 21:43:12 -0400

    resolved issue #8

--- src/EasyKeys.Extensions.Storage.SFtp/SFtpBlobStorageOptions.cs
+++ src/EasyKeys.Extensions.Storage.SFtp/SFtpBlobStorageOptions.cs
@@ -4,12 +4,29 @@ namespace EasyKeys.Extensions.Storage.SFtp
 {
     public class SFtpBlobStorageOptions : BlobStorageOptions
     {
+        /// <summary>
+        /// The name of the host. The default is empty.
+        /// </summary>
         public string Host { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The sftp port number. The default is 22.
+        /// </summary>
         public int Port { get; set; } = 22;
 
+        /// <summary>
+        /// The user name for login.
+        /// </summary>
         public string UserName { get; set; } = string.Empty;
 
+        /// <summary>
+        /// If  password is empty and Private Key is present then it uses SSH2 ENCRYPTED PRIVATE KEY.
+        /// </summary>
         public string Password { get; set; } = string.Empty;
+
+        /// <summary>
+        /// OpenSSH SSH2 ENCRYPTED PRIVATE KEY.
+        /// </summary>
+        public string PrivateKey { get; set; } = string.Empty;
     }
 }
